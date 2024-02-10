972f7a4b0e3ed8b9f59a1dcef390e1373f7c84cf
pranayjain099 <pranayjain2001@gmail.com>
2024-02-10 15:02:21 +0530

    registered and enqueued the scripts and styles

--- functions.php
+++ functions.php
@@ -4,8 +4,12 @@
 
 function unisus_enqueue_scripts()
 {
+    // Registering styles and scripts
+    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
 
-    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_style('style-css');
+    wp_enqueue_script('style-css');
 }
 
 add_action('wp_enqueue_scripts', 'unisus_enqueue_scripts');
