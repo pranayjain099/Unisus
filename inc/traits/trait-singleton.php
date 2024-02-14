2fa0b93f5b9c344f36962e4e6f6174259a449c8c
pranayjain099 <pranayjain2001@gmail.com>
2024-02-14 11:48:06 +0530

    Trait-Singleton created

---
+++ inc/traits/trait-singleton.php
@@ -0,0 +1,31 @@
+<?php
+
+trait singleton
+{
+    public function __construct()
+    {
+
+    }
+
+    public function __clone()
+    {
+
+    }
+
+    // This function will return the single instance of the class.
+    final public static function getInstance()
+    {
+        static $instance = [];
+        $class_Name = get_called_class();
+
+        if (!isset($instance[$class_Name])) {
+            $instance[$class_Name] = new $class_Name();
+
+            // if any plugin want to hook at this point 
+
+            do_action(sprintf('unisus_theme_singleton_init%s'), $class_Name);
+        }
+
+        return $instance[$class_Name];
+    }
+}
