5143b90ccad6bf9aad6a4f0fb54be5da98649ead
pranayjain099 <pranayjain2001@gmail.com>
2024-02-19 18:37:41 +0530

    custom menu created and styled in navbar

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -33,5 +33,37 @@ class Menus
         ]);
     }
 
+    public function get_menu_id($location)
+    {
+        // get all the locations
+        $locations = get_nav_menu_locations();
+
+        // get object id by location
+        $menu_id = $locations[$location];
+
+        // Return menu_id if not empty else don't return anything.
+        return !empty($menu_id) ? $menu_id : '';
+
+
+    }
+
+    public function get_child_menu_items($menu_array, $parent_id)
+    {
+        // we will store all child menu in array first
+        $child_menus = [];
+
+        if (!empty($menu_array) && is_array($menu_array)) {
+
+            foreach ($menu_array as $menu) {
+                if (intval($menu->menu_item_parent) === $parent_id) {
+                    // automatically pushes into array you are working with.
+                    array_push($child_menus, $menu);
+                }
+            }
+        }
+
+        return $child_menus;
+    }
+
 
 }
