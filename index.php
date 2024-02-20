bcab0e582eb43db5c7a584afa89f4d19c97f3e0f
pranayjain099 <pranayjain2001@gmail.com>
2024-02-20 17:45:34 +0530

    Post content and title displayed in blog page

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
 get_header();
 ?>
 
-<div class="content">
-    <?php esc_html_e('Content Index', 'Unisus'); ?>
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <!-- extracting post -->
+        <?php
+        if (have_posts()) {
+            ?>
+            <div class="container">
+                <?php
+                while (have_posts()):
+                    the_post();
+                    the_title();
+                    the_content();
+                endwhile;
+                ?>
+            </div>
+            <?php
+        }
+        ?>
+    </main>
 </div>
+
 
 <?php get_footer();
