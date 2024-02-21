c41b03008d1acbcf0b94e5fc847e6a94ef94dcbb
pranayjain099 <pranayjain2001@gmail.com>
2024-02-21 21:57:38 +0530

    components / blog created

--- template-parts/content.php
+++ template-parts/content.php
@@ -5,6 +5,17 @@
 
 ?>
 
+<article id="post-<?php the_ID(); ?>" <?php post_class('mb-5'); ?>>
+
+    <?php
+    get_template_part('template-parts/components/blog/entry-header');
+    get_template_part('template-parts/components/blog/entry-metar');
+    get_template_part('template-parts/components/blog/entry-content');
+    get_template_part('template-parts/components/blog/entry-footer');
+    ?>
+
+</article>
+
 <h3>
     <?php the_title(); ?>
 </h3>
