005556938dda65f9320b8390532f7553a66df53e
pranayjain099 <pranayjain2001@gmail.com>
2024-03-14 20:01:00 +0530

    Clean code pratice

---
+++ template-parts/patterns/cover.php
@@ -0,0 +1,35 @@
+<?php
+
+// Cover pattern
+// @package Unisus
+
+?>
+
+<!-- wp:cover {"url":"http://localhost:8080/Theme_1/wp-content/uploads/2024/03/yash-raut-z_zFPGX90mc-unsplash.jpg",
+                                "id":119,"dimRatio":50,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img
+        class="wp-block-cover__image-background wp-image-119" alt=""
+        src="http://localhost:8080/Theme_1/wp-content/uploads/2024/03/yash-raut-z_zFPGX90mc-unsplash.jpg"
+        data-object-fit="cover" />
+    <div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1} -->
+        <h1 class="wp-block-heading has-text-align-center">Jodhpur</h1>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|cyan-bluish-gray"}}}},"textColor":"cyan-bluish-gray"} -->
+        <p class="has-text-align-center has-cyan-bluish-gray-color has-text-color has-link-color">Mehrangarh is a
+            historic fort located in Jodhpur, Rajasthan, India. It stands on a hilltop, rising about 122 meters above
+            the surrounding plains. The complex spans 1,200 acres</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:buttons {"align":"full","layout":{"type":"flex","justifyContent":"center"}} -->
+        <div class="wp-block-buttons alignfull">
+            <!-- wp:button {"style":{"color":{"gradient":"linear-gradient(90deg,rgb(2,3,129) 0%,rgb(40,116,252) 97%)"},"border":{"radius":"100px"}},"className":"is-style-fill"} -->
+            <div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-background wp-element-button"
+                    style="border-radius:100px;background:linear-gradient(90deg,rgb(2,3,129) 0%,rgb(40,116,252) 97%)">Blogs</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:buttons -->
+    </div>
+</div>
+<!-- /wp:cover -->
