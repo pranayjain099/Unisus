2024e2478504b5d6db9a7a3087d0bfc01572d52f
pranayjain099 <pranayjain2001@gmail.com>
2024-03-14 12:20:23 +0530

    Added block pattern categories
    register_block_pattern

--- inc/classes/class-block-patterns.php
+++ inc/classes/class-block-patterns.php
@@ -22,6 +22,7 @@ class Block_Patterns
     {
         //Actions
         add_action('init', [$this, 'register_block_patterns']);
+        add_action('init', [$this, 'register_block_patterns_categories']);
     }
 
     public function register_block_patterns()
@@ -30,8 +31,16 @@ class Block_Patterns
             register_block_pattern(
                 'Unisus/cover',
                 [
+                    // Title of the block pattern 
                     'title' => __('Unisus Cover', 'Unisus'),
+
+                    // Description of the block pattern
                     'description' => __('Unisus Cover Block with image and text', 'Unisus'),
+
+                    // Category of this block pattern
+                    'categories' => ['cover'],
+
+                    // Raw html
                     'content' => '<!-- wp:cover {"url":"http://localhost:8080/Theme_1/wp-content/uploads/2024/03/yash-raut-z_zFPGX90mc-unsplash.jpg",
                                 "id":119,"dimRatio":50,"layout":{"type":"constrained"}} -->
                                 <div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-119" alt="" src="http://localhost:8080/Theme_1/wp-content/uploads/2024/03/yash-raut-z_zFPGX90mc-unsplash.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1} -->
@@ -54,5 +63,24 @@ class Block_Patterns
         }
     }
 
+
+    // Registering block pattern categories
+    public function register_block_patterns_categories()
+    {
+        // Categories 
+        $pattern_categories = [
+            'cover' => __('Cover', 'Unisus'),
+            'two-columns' => __('Two-columns', 'Unisus'),
+        ];
+
+        if (!empty($pattern_categories) && is_array($pattern_categories)) {
+            foreach ($pattern_categories as $pattern_category => $pattern_category_label) {
+                register_block_pattern_category(
+                    $pattern_category,
+                    ['label' => $pattern_category_label]
+                );
+            }
+        }
+    }
 
 }
